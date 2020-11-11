a4a2a00871d03d9566f7925b194827789f31f035
kamnev <sapphire.s@bk.ru>
2020-11-11 14:14:53 +0300

    имя контейнера + валидация

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Container;
+use App\Log;
 use App\Status;
 use Illuminate\Http\Request;
 use App\Http\Resources\ContainerWithRelationshipsResource;
@@ -10,24 +11,6 @@ use Illuminate\Support\Facades\Validator;
 
 class ContainerController extends Controller
 {
-    protected function containerCreateValidator(array $data)
-    {
-        $messages = [
-            'required' => 'Поле :attribute обязательно для заполнения.',
-            'max' => 'Поле :attribute должно содержать не более :max символов',
-        ];
-
-        $names = [
-            'name' => 'название контейнера',
-            'city' => 'город',
-        ];
-
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'city' => ['required', 'string', 'max:255'],
-        ], $messages, $names);
-    }
-
     /**
      * Display a listing of the resource.
      *
@@ -47,13 +30,15 @@ class ContainerController extends Controller
      */
     public function store(Request $request, Container $container)
     {
-        $this->containerCreateValidator($request->all())->validate();
         $container->name = $request->input('name');
         $container->status = array_keys(get_object_vars(Status::getContainerStatuses()), 'Создан')[0];
         $orderIds = $request->input('orders');
         $container->quantity_order_items = $container->getQuantityOrderItems($orderIds);
         $container->save();
         $container->addOrders($orderIds);
+        Log::$write = false;
+        $container->name = $container->id;
+        $this->save();
         return response()->json(new ContainerWithRelationshipsResource($container), 201);
     }
 
@@ -77,7 +62,6 @@ class ContainerController extends Controller
      */
     public function update(Request $request, Container $container)
     {
-        $this->containerCreateValidator($request->all())->validate();
         $container->update($container->dashesToSnakeCase($request->all()));
         return response()->json(new ContainerWithRelationshipsResource($container), 200);
     }
