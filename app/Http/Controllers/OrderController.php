8649b329d9c508e0a966cf4163706b469913d910
kamnev <sapphire.s@bk.ru>
2020-11-16 18:24:58 +0300

    проверка на старые документов у старых заказов

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\City;
+use App\ContractDocument;
 use App\Http\Resources\ProductResource;
 use App\Importer;
+use App\InvoiceDocument;
 use App\Order;
 use App\OrderItem;
 use App\Product;
+use App\ProformaDocument;
 use App\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -197,6 +200,11 @@ class OrderController extends Controller
         $importer = Importer::first();
         $provider = $order->provider;
 
+        if (!$order->contract) {
+            $contract = new ContractDocument();
+            $contract->order_id = $order->id;
+            $contract->save();
+        }
         $order->contract->saveInfoWithJson($request->all());
         $contract = $order->contract->getInfo();
 
@@ -237,6 +245,11 @@ class OrderController extends Controller
         $importer = Importer::first();
         $provider = $order->provider;
 
+        if (!$order->proforma) {
+            $proforma = new ProformaDocument();
+            $proforma->order_id = $order->id;
+            $proforma->save();
+        }
         $order->proforma->saveInfoWithJson($request->all());
         $proforma = $order->proforma->getInfo();
         $contract = $order->contract->getInfo();
@@ -273,6 +286,11 @@ class OrderController extends Controller
         $importer = Importer::first();
         $provider = $order->provider;
 
+        if (!$order->invoice) {
+            $invoice = new InvoiceDocument();
+            $invoice->order_id = $order->id;
+            $invoice->save();
+        }
         $order->invoice->saveInfoWithJson($request->all());
         $invoice = $order->invoice->getInfo();
         $proforma = $order->proforma->getInfo();
