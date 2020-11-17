f3e8eea6d5c1e77450ceaefb6b7874bf3315a6ff
lusyio <mr-kelevras@yandex.ru>
2020-11-17 19:55:16 +0300

    версия

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,10 +8,10 @@
     <title>Autolong</title>
     <link rel="shortcut icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml"/>
     <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml"/>
-    <link href="{{ asset('css/app.css?v=0.1.3') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css?v=0.1.4') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id="root"></div>
-<script src="{{ asset('js/app.js?v=0.1.4') }}"></script>
+<script src="{{ asset('js/app.js?v=0.1.5') }}"></script>
 </body>
 </html>
