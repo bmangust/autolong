35fb8c9b2cf8be4aafb6c24fe6c36486fde266ac
kamnev <sapphire.s@bk.ru>
2020-11-23 18:02:30 +0300

    миддл

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    
+
     Route::post('/logout', 'LoginController@logout');
 
     Route::get('importers', 'ImporterController@index');
