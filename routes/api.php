c910b84923577bb4fc5f20a66d22d564073c2e58
kamnev <sapphire.s@bk.ru>
2020-11-23 16:47:53 +0300

    роут

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'LoginController@login');
+Route::post('/login', 'LoginController@login')->name('login');
 Route::post('/logout', 'LoginController@logout');
 
 
