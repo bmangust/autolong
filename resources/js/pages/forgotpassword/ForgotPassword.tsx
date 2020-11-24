ce0b647971bf81e41a4a174a99009c74b739a8ec
skripkaru <skripkaru.evgeniy@gmail.com>
2020-11-24 12:56:40 +0300

    Авторизация + забыли пароль

--- resources/js/pages/forgotpassword/ForgotPassword.tsx
+++ resources/js/pages/forgotpassword/ForgotPassword.tsx
@@ -26,36 +26,34 @@ const ForgotPassword: React.FC = () => {
             .catch(() => toast.error('Что-то пошло не так'))
     }
     return (
-        <div className={classes.ForgotPassword}>
-            <div className='row'>
-                <div className='col-xl-7'>
-                    <form onSubmit={forgotHandler}
-                          className={classes.ForgotPasswordForm}>
-                        <h1>Забыли пароль?</h1>
-                        <p>
-                            Введите свой адрес электронной почты и нажмите
-                            кнопку восстановить, чтобы получить новый пароль
-                        </p>
-                        <input
-                            name='email'
-                            type='email'
-                            onChange={(e) => setEmail(e.target.value)}
-                            autoComplete='email'
-                            placeholder='Введите свой e-mail'
-                            required
-                        />
-                        <button
-                            type='submit'
-                            className='btn btn-success w-100'>
-                            Восстановить
-                        </button>
-                        <NavLink
-                            className='d-table ml-auto mr-auto'
-                            to={'/login'}>
-                            Вернуться назад
-                        </NavLink>
-                    </form>
-                </div>
+        <div className={classes.ForgotPassword + ' row'}>
+            <div className='col-xl-7 justify-content-center'>
+                <form onSubmit={forgotHandler}
+                        className={classes.ForgotPasswordForm}>
+                    <h1>Забыли пароль?</h1>
+                    <p>
+                        Введите свой адрес электронной почты и нажмите
+                        кнопку восстановить, чтобы получить новый пароль
+                    </p>
+                    <input
+                        name='email'
+                        type='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        autoComplete='email'
+                        placeholder='Введите свой e-mail'
+                        required
+                    />
+                    <button
+                        type='submit'
+                        className='btn btn-success w-100'>
+                        Восстановить
+                    </button>
+                    <NavLink
+                        className='d-table ml-auto mr-auto'
+                        to={'/login'}>
+                        Вернуться назад
+                    </NavLink>
+                </form>
             </div>
         </div>
     )
