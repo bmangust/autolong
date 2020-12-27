893bb09007ccc6c041d586d9373d7f2ffacfc6f6
kamnev <sapphire.s@bk.ru>
2020-12-27 13:35:05 +0300

    Триггер для новинок

---
+++ database/migrations/2020_12_27_132052_add_trigger_to_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddTriggerToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $userName = env('DB_USERNAME', 'root');
+        $databaseHost = env('DB_HOST', 'localhost');
+        $database = env('DB_DATABASE', 'laravel');
+        DB::unprepared("
+              CREATE DEFINER=`$userName`@`$databaseHost` TRIGGER `$database`.`products_AFTER_INSERT` AFTER INSERT ON `products` FOR EACH ROW
+                  BEGIN
+                        IF NEW.published = 0 THEN
+                            INSERT INTO 1c_sandbox.autolong_erp_new_products (name_ru, name_en, about_ru, about_en, price_rub, price_usd, price_cny, weight_netto, weight_brutto)
+                            VALUES (NEW.name_ru, NEW.name_en, NEW.about_ru, NEW.about_en, NEW.price_rub, NEW.price_usd, NEW.price_cny, NEW.weight_netto, NEW.weight_brutto);
+                        END IF;
+                  END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $database = env('DB_DATABASE', 'laravel');
+        DB::unprepared("
+            DROP TRIGGER IF EXISTS $database.products_AFTER_INSERT
+        ");
+    }
+}
