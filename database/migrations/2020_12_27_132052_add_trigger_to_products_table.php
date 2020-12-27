7f7458cd503c3bfb6ec81c0adb55d7820c8e3c39
kamnev <sapphire.s@bk.ru>
2020-12-27 14:09:00 +0300

    Вставка триггера при необходимости

--- database/migrations/2020_12_27_132052_add_trigger_to_products_table.php
+++ database/migrations/2020_12_27_132052_add_trigger_to_products_table.php
@@ -12,17 +12,20 @@ class AddTriggerToProductsTable extends Migration
      */
     public function up()
     {
-        $userName = env('DB_USERNAME', 'root');
-        $database = env('DB_DATABASE', 'laravel');
-        DB::unprepared("
+        if (env('DB_ADD_TRIGGER', false)) {
+            $userName = env('DB_USERNAME', 'root');
+            $database = env('DB_DATABASE', 'laravel');
+            $databaseToInsertNew = env('DB_TO_INSERT_NEW_PRODUCTS', '');
+            DB::unprepared("
               CREATE DEFINER=`$userName`@`localhost` TRIGGER `$database`.`products_AFTER_INSERT` AFTER INSERT ON `products` FOR EACH ROW
                   BEGIN
                         IF NEW.published = 0 THEN
-                            INSERT INTO 1c_sandbox.autolong_erp_new_products (name_ru, name_en, about_ru, about_en, price_rub, price_usd, price_cny, weight_netto, weight_brutto)
+                            INSERT INTO 1c_sandbox.$databaseToInsertNew (name_ru, name_en, about_ru, about_en, price_rub, price_usd, price_cny, weight_netto, weight_brutto)
                             VALUES (NEW.name_ru, NEW.name_en, NEW.about_ru, NEW.about_en, NEW.price_rub, NEW.price_usd, NEW.price_cny, NEW.weight_netto, NEW.weight_brutto);
                         END IF;
                   END
         ");
+        }
     }
 
     /**
@@ -32,9 +35,11 @@ class AddTriggerToProductsTable extends Migration
      */
     public function down()
     {
-        $database = env('DB_DATABASE', 'laravel');
-        DB::unprepared("
-            DROP TRIGGER IF EXISTS $database.products_AFTER_INSERT
-        ");
+        if (env('DB_ADD_TRIGGER', false)) {
+            $database = env('DB_DATABASE', 'laravel');
+            DB::unprepared("
+                DROP TRIGGER IF EXISTS $database.products_AFTER_INSERT
+            ");
+        }
     }
 }
