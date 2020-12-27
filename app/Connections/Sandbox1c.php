9c9f37e7e4fdcf30fcb94257323fab9e169dc718
kamnev <sapphire.s@bk.ru>
2020-12-27 17:59:16 +0300

    Доработка присваивания ключа новым товарам

--- app/Connections/Sandbox1c.php
+++ app/Connections/Sandbox1c.php
@@ -3,7 +3,6 @@
 
 namespace App\Connections;
 
-
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -11,14 +10,23 @@ class Sandbox1c
 {
     private const CONNECTION = 'mysql_1c_sandbox';
     private const PRODUCTS_TABLE = 'products';
+    private const AUTOLONG_ERP_NEW_PRODUCTS_TABLE = 'autolong_erp_new_products';
     private const TO_ENCODING = 'utf8';
     private const FROM_ENCODING = 'cp1251';
 
     public static function getBiggestAutolongNumber()
     {
+        $productTable = self::PRODUCTS_TABLE;
+        $autolongTable = self::AUTOLONG_ERP_NEW_PRODUCTS_TABLE;
         try {
-            return mb_convert_encoding(DB::connection(self::CONNECTION)->table(self::PRODUCTS_TABLE)->orderByDesc('id')->first()->id_1c,
-                    self::TO_ENCODING, self::FROM_ENCODING) + 1;
+            $code = DB::connection(self::CONNECTION)
+                            ->select("SELECT code FROM $productTable order by CAST(code as UNSIGNED) DESC LIMIT 1;")[0]->code + 1;
+            $latestCodeInAutolongErp = DB::connection(self::CONNECTION)
+                            ->select("SELECT autolong_number FROM $autolongTable order by CAST(autolong_number as UNSIGNED) DESC LIMIT 1;")[0]->autolong_number;
+            if ($code == $latestCodeInAutolongErp) {
+                ++$code;
+            }
+            return $code;
         } catch (HttpException $exception) {
             throw new HttpException(500, 'Не удалось получить информацию из базы данных 1с_sandbox.');
         }
