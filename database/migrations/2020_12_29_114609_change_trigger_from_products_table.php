9aeea48db102b2ce282efaf42c9fee70634af424
kamnev <sapphire.s@bk.ru>
2020-12-29 13:26:41 +0300

    переменная триггера

--- database/migrations/2020_12_29_114609_change_trigger_from_products_table.php
+++ database/migrations/2020_12_29_114609_change_trigger_from_products_table.php
@@ -27,9 +27,9 @@ class ChangeTriggerFromProductsTable extends Migration
               CREATE DEFINER=`$userName`@`localhost` TRIGGER `$database`.`products_BEFORE_UPDATE` BEFORE UPDATE ON `products` FOR EACH ROW
                   BEGIN
                     IF OLD.published = 0 AND NEW.published = 1 THEN
-                        SET NEW.image = CONCAT('$fullPathImage', NEW.image);
+                        SET @full_path := CONCAT('$fullPathImage', NEW.image);
                         INSERT INTO 1c_sandbox.$databaseToInsertNew (name_ru, name_en, about_ru, about_en, price_rub, price_usd, price_cny, weight_netto, weight_brutto, image, autolong_number, vendor_code)
-                        VALUES (NEW.name_ru, NEW.name_en, NEW.about_ru, NEW.about_en, NEW.price_rub, NEW.price_usd, NEW.price_cny, NEW.weight_netto, NEW.weight_brutto, NEW.image, NEW.autolong_number, NEW.vendor_code);
+                        VALUES (NEW.name_ru, NEW.name_en, NEW.about_ru, NEW.about_en, NEW.price_rub, NEW.price_usd, NEW.price_cny, NEW.weight_netto, NEW.weight_brutto, full_path, NEW.autolong_number, NEW.vendor_code);
                     END IF;
                   END
         ");
