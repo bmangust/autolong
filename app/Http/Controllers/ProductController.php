f38ef8a3012402d70cdcde4ae533c1efc73d9a3a
kamnev <sapphire.s@bk.ru>
2021-02-01 12:58:11 +0300

    рефреш

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -127,6 +127,7 @@ class ProductController extends Controller
         $product->autolong_number = $request->input('autolongNumber');
         $product->hs_code = $request->input('hsCode') ?? 0;
         $product->save();
+        $product->refresh();
         return response()->json(new ProductWithRelationshipsResource($product), 200);
     }
 
