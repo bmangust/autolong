0456c81fcbbad2fc994e5a2a3fa593ee0552281c
Zakhar Vozmilov <zaharus000@mail.ru>
2022-06-17 16:25:41 +0700

    fix schedule for ozon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -105,7 +105,7 @@ class Kernel extends ConsoleKernel
             }
         })->hourly();
 
-        $schedule->command(SyncOzonStocks::class)->hourly();
+        $schedule->command(SyncOzonStocks::class)->everyMinute();
     }
 
     /**
